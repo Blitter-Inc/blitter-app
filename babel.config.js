323a8e2337a3ca6c740f9cdd9ef421bd6a0ef46a
Pranav Jain <pranav19sept@gmail.com>
2021-09-11 20:49:49 +0530

    Added expo-secure-store and firebase libs Configured project for path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/app/components',
+            '@navigations': './src/app/navigations',
+            '@screens': './src/app/screens',
+          },
+        },
+      ],
+    ],
   };
 };
