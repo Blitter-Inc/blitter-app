e3bc5a90bf02c52ef5e6cd29a4a881fa0c82b702
Pranav Jain <pranav19sept@gmail.com>
2021-09-14 20:37:19 +0530

    Removed console.log

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -1,7 +1,7 @@
 import Firebase from 'firebase/app';
 import { FIREBASE_CONFIG } from './env';
 
-console.log(FIREBASE_CONFIG);
+
 Firebase.initializeApp(FIREBASE_CONFIG);
 
 export default Firebase;
