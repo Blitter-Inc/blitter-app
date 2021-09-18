922e325c7cbe4c09239d42f96722a083bc2a3b0d
Pranav Jain <pranav19sept@gmail.com>
2021-09-18 22:37:57 +0530

    Using hooks to fetch state in RootNavigator

--- src/app/navigations/RootNavigator.tsx
+++ src/app/navigations/RootNavigator.tsx
@@ -1,32 +1,36 @@
 import React, { FC } from 'react';
-import { connect } from 'react-redux';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SignInScreen, OTPVerifyScreen } from '@screens/auth';
 import { HomeScreen } from '@screens/home';
+import { useAppSelector } from '@store/hooks';
 
+
+const fetchRequiredState = () => {
+  const authState = useAppSelector(state => state.auth);
+  return {
+    isAuthenticated: authState.credentials.accessToken != null,
+  };
+}
 
 const Stack = createStackNavigator();
 
-const RootNavigator: FC = ({ state: { isAuthenticated } }) => (
-  <Stack.Navigator screenOptions={{ headerShown: false }}>
-    {isAuthenticated ? (
-      <>
-        <Stack.Screen name='Home' component={HomeScreen} />
-      </>
-    ) : (
-      <>
-        <Stack.Screen name='SignIn' component={SignInScreen} />
-        <Stack.Screen name='OTPVerify' component={OTPVerifyScreen} />
-      </>
-    )}
-  </Stack.Navigator>
-);
-
-const mapStateToProps = state => ({
-  state: {
-    isAuthenticated: state.auth.credentials.accessToken != null,
-  },
-});
+const RootNavigator: FC = () => {
+  const { isAuthenticated } = fetchRequiredState();
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {isAuthenticated ? (
+        <>
+          <Stack.Screen name='Home' component={HomeScreen} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name='SignIn' component={SignInScreen} />
+          <Stack.Screen name='OTPVerify' component={OTPVerifyScreen} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+};
 
 
-export default connect(mapStateToProps)(RootNavigator);
+export default RootNavigator;
